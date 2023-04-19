90a7a7e5e66e525e4b72e7be1213e96ee8ea8f82
Jawahar96 <jawaharsabesan@gmail.com>
2023-04-19 06:23:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3001
+const bodyParser = require('body-parser')
+
+const mongoose = require('mongoose')
+
+const DB='myUrlShortener'
+
+mongoose.connect("mongodb+srv://jawaharsabesan:63EPXZmgxVcFuVXh@cluster0.n6dtkg8.mongodb.net/?retryWrites=true&w=majority");
+
+const { UrlModel } = require('./models/urlshort')
+
+// middleware
+app.use(express.static('public'))
+app.set('view engine', "ejs")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/',   function(req, res) {
+    let allUrl =   UrlModel.find(function (err, result) {
+        res.render('home', {
+            urlResult: result
+        })
+
+    })
+
+})
+
+app.post('/create', function (req, res) {
+
+    let urlshort = new UrlModel({
+        longUrl: req.body.longurl,
+        shortUrl: generateUrl()
+    })
+
+    urlshort.save(function (err, data) {
+        if (err) throw err
+        console.log(data);
+        res.redirect('/')
+
+    })
+})
+app.get('/:urlId', function (req, res) {
+
+    let urlshort = UrlModel.findOne({ shortUrl: req.params.urlId }, function (err, data) {
+        if (err) throw err
+
+        UrlModel.findByIdAndUpdate({ _id: data.id }, { $inc: { clickCount: 1 } }, function (err, updateddata) {
+            if (err) throw err
+            res.redirect(data.longUrl)
+            console.log(req.params.urlId);
+        })
+    })
+    
+})
+
+app.get('/delete/:id',function(req,res){
+    UrlModel.findByIdAndDelete({_id :req.params.id},function(err,deletedata){
+        if(err) throw (err)
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON THE PORT ${PORT}`);
+})
+
+function generateUrl() {
+    var rndResult = ""
+    var character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    var charactersLength = character.length;
+
+    for (let i = 0; i < 5; i++) {
+        rndResult = rndResult + character.charAt(Math.floor(Math.random() * charactersLength))
+    }
+}
