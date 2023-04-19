83d3606be1fee43564f61543969a00d7f836b62c
Jawahar96 <jawaharsabesan@gmail.com>
2023-04-19 07:47:10 +0530

    1updat

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.set('view engine', "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.get('/',   function(req, res) {
-    let allUrl =   UrlModel.find(function (err, result) {
+app.get('/', function (req, res) {
+    let allUrl = UrlModel.find(function (err, result) {
         res.render('home', {
             urlResult: result
         })
@@ -45,7 +45,7 @@ app.post('/create', function (req, res) {
 })
 app.get('/:urlId', function (req, res) {
 
-     UrlModel.findOne({ shortUrl: req.params.urlId }, function (err, data) {
+    UrlModel.findOne({ shortUrl: req.params.urlId }, function (err, data) {
         if (err) throw err
 
         UrlModel.findByIdAndUpdate({ _id: data.id }, { $inc: { clickCount: 1 } }, function (err, updateddata) {
@@ -54,12 +54,12 @@ app.get('/:urlId', function (req, res) {
             console.log(req.params.urlId);
         })
     })
-    
+
 })
 
-app.get('/delete/:id',function(req,res){
-    UrlModel.findByIdAndDelete({_id :req.params.id},function(err,deletedata){
-        if(err) throw (err)
+app.get('/delete/:id', function (req, res) {
+    UrlModel.findByIdAndDelete({ _id: req.params.id }, function (err, deletedata) {
+        if (err) throw (err)
     })
 })
 
@@ -69,11 +69,15 @@ app.listen(PORT, () => {
 })
 
 function generateUrl() {
-    var rndResult = ""
+
+    var rndResult = "";
     var character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     var charactersLength = character.length;
 
     for (let i = 0; i < 5; i++) {
-        rndResult = rndResult + character.charAt(Math.floor(Math.random() * charactersLength))
+        rndResult = rndResult + character.charAt(Math.floor(Math.random() * charactersLength));
     }
+    
+return rndResult
 }
+
