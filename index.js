fcd1cfba130cc3f25bd5e7db58ca99ab39cae83d
Jawahar96 <jawaharsabesan@gmail.com>
2023-04-23 10:36:03 +0530

    4 update

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const bodyParser = require('body-parser')
 
 const mongoose = require('mongoose')
 
-const url =('mongodb://localhost:27017')
+const url =('mongodb://localhost:27017/myUrlShortener')
 
 
 
 const { UrlModel } = require('./models/urlshort')
-const { MongoClient } = require('mongodb')
+
 
 // middleware
 app.use(express.static('public'))
@@ -20,52 +20,58 @@ app.set('view engine', "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.get('/', function (req, res) {
-    let allUrl = UrlModel.find(function (err, result) {
+app.get('/', async function (req, res) {
+    let allUrl =  await UrlModel.find(function (err, result) {
         res.render('home', {
             urlResult: result
+            
         })
-
+ console.log(allUrl)
     })
 
 })
 
-app.post('/create', function (req, res) {
+app.post('/create',  function (req, res) {
 
-    let urlshort = new UrlModel({
+    let urlShort = new UrlModel({
         longUrl: req.body.longurl,
         shortUrl: generateUrl()
     })
 
-    urlshort.save(function (err, data) {
+    urlShort.save(function (err, data) {
         if (err) throw err
         console.log(data);
         res.redirect('/')
-
+ 
     })
 })
-app.get('/:urlId', function (req, res) {
+app.get('/:urlId',  async function (req, res) {
 
-    UrlModel.findOne({ shortUrl: req.params.urlId }, function (err, data) {
+   let url = await UrlModel.findOne({ shortUrl: req.params.urlId }, function (err, data) {
         if (err) throw err
 
-        UrlModel.findByIdAndUpdate({ _id: data.id }, { $inc: { clickCount: 1 } }, function (err, updateddata) {
+        UrlModel.findByIdAndUpdate({ _id: data.id }, { $inc: { clickCount: 1 } }, function (err, updatedData) {
             if (err) throw err
+            
             res.redirect(data.longUrl)
             console.log(req.params.urlId);
+            return data
         })
     })
 
 })
 
 app.get('/delete/:id', function (req, res) {
-    UrlModel.findByIdAndDelete({ _id: req.params.id }, function (err, deletedata) {
+    UrlModel.findByIdAndDelete({ _id: req.params.id }, function (err, deleteData) {
         if (err) throw (err)
+        res.redirect('/')
     })
 })
 
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000,function(){
+    console.log(`PORT IS RUNNING IN 3000 `);
+})
 
 function generateUrl() {
 
